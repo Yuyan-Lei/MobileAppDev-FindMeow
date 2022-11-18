3e308dc9d0bdf1caa19bd626a97025ac0c95f996
CuichanWu <wu.cu@northeastern.edu>
2022-11-17 23:43:03 -0800

    fetch: Cat information main

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { FilterButtons } from "./components/pressable/FilterButtons";
 import { useState } from "react";
 import DiscoverMainScreen from "./components/screens/DiscoverMainScreen";
 import DiscoverFilter from "./components/screens/DiscoverFilter";
+import CatInformation from "./components/screens/CatInformation";
 
 // only use for test before adding a real navigation bar
 export default function App() {
   return (
     <View>
       {/* <DiscoverMainScreen /> */}
-      <DiscoverFilter></DiscoverFilter>
+      {/* <DiscoverFilter></DiscoverFilter> */}
+      <CatInformation />
     </View>
   );
 }
