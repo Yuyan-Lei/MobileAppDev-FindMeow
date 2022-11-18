b0e893a4eb9b8a57bffc8d2cee01fcdccd693de2
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-11-17 19:36:43 -0800

    chore: change mock data

--- components/screens/DiscoverMainScreen.jsx
+++ components/screens/DiscoverMainScreen.jsx
@@ -8,20 +8,20 @@ import { TitleText } from "../texts/TitleText";
 const cats = [
   { name: "aaa", month: 3, sex: "Male", location: "San Jose", price: 1000 },
   { name: "bbb", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
-  { name: "aaa", month: 3, sex: "Male", location: "San Jose", price: 1000 },
-  { name: "bbb", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
-  { name: "aaa", month: 3, sex: "Male", location: "San Jose", price: 1000 },
-  { name: "bbb", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
-  { name: "aaa", month: 3, sex: "Male", location: "San Jose", price: 1000 },
-  { name: "bbb", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
-  { name: "aaa", month: 3, sex: "Male", location: "San Jose", price: 1000 },
-  { name: "bbb", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
+  { name: "ccc", month: 3, sex: "Male", location: "San Jose", price: 1000 },
+  { name: "ddd", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
+  { name: "eee", month: 3, sex: "Male", location: "San Jose", price: 1000 },
+  { name: "fff", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
+  { name: "ggg", month: 3, sex: "Male", location: "San Jose", price: 1000 },
+  { name: "hhh", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
+  { name: "iii", month: 3, sex: "Male", location: "San Jose", price: 1000 },
+  { name: "ooo", month: 5, sex: "Female", location: "Palo Alto", price: 1500 },
 ];
 
 export default function DiscoverMainScreen() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <View style={{ margin: 16, marginTop: 28 }}>
+    <View style={{ margin: 16, marginTop: 28, marginBottom: 200 }}>
       <View style={{ flexDirection: "row", margin: 12 }}>
         <View style={{ flex: 7 }}>
           <TitleText>Discover</TitleText>
