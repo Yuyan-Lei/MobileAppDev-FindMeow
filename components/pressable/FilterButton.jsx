ec712b93d7950b7eb23156adeeed8e09b61f9927
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-11-18 01:25:46 -0800

    feat: change icon

--- components/pressable/FilterButton.jsx
+++ components/pressable/FilterButton.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { ButtonGroup, Button } from "@rneui/themed";
+import { ButtonGroup, Button, Icon } from "@rneui/themed";
 import { Text, StyleSheet, View } from "react-native";
 
 // reference: https://reactnativeelements.com/docs/components/buttongroup
 export function FilterButton() {
   return (
-    <View style={{ width: 40, height: 32 }}>
-      <Button title="↓↑" titleStyle={{ fontSize: 16 }} />
+    <View style={{ width: 60, height: 40, top: 4 }}>
+      {/* <Button title="↓↑" titleStyle={{ fontSize: 16 }} /> */}
+      <Icon name="setting" type="antdesign" color="blue" />
     </View>
   );
 }
