d80c92bc5cae3de1a55186aa1a49f229e3a6845b
Yuyan Lei <lei.yuy@northeastern.edu>
2022-11-19 15:16:20 -0800

    feat: Add icons & buttons in navigation bar

--- components/screens/HomePage.jsx
+++ components/screens/HomePage.jsx
@@ -8,29 +8,59 @@ import { Dimensions } from "react-native";
 import DiscoverFilter from "./DiscoverFilter";
 import CatInformation from "./CatInformation";
 import PostNewCatScreen from "./PostNewCatScreen";
+import { Ionicons } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function HomePage({ route, navigation }) {
   const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator
-      tabBar={(props) => <TabBar {...props} />}
-      screenOptions={{ headerShown: false }}
+      screenOptions={({route}) => ({ 
+        headerShown: false,
+        tabBarActiveTintColor: '#F6A939',
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Post') {
+            return <Ionicons name='add-circle-outline' size={size} color={color} />;
+          } else if (route.name === 'Cats') {
+            return <FontAwesome5 name='cat' size={size} color={color} />;
+          } else if (route.name === 'Catteries') {
+            return <MaterialIcons name='storefront' size={size} color={color} />;
+          } else if (route.name === 'Like') {
+            return <Ionicons name='heart-outline' size={size} color={color} />;
+          } else {
+            return <AntDesign name="user" size={size} color={color} />;
+          }
+        },
+       })}
     >
       <Tab.Screen
-        name="DiscoverMain"
+        name="Cats"
         component={DiscoverMainScreen}
         initialParams={{ symbol: "1" }}
       />
       <Tab.Screen
-        name="FindBreederMain"
+        name="Catteries"
         component={FindBreederMainScreen}
         initialParams={{ symbol: "2" }}
       />
       <Tab.Screen
-        name="PostNewCat"
+        name="Post"
         component={PostNewCatScreen}
-        initialParams={{ symbol: "3"}}
+        initialParams={{ symbol: "3" }}
+        screenOptions={{ tabBarLabel: "Add"}}
+      />
+      <Tab.Screen
+        name="Like"
+        component={PostNewCatScreen}
+        initialParams={{ symbol: "4" }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={PostNewCatScreen}
+        initialParams={{ symbol: "5" }}
       />
       {/* <Tab.Screen
         name="CatteryProfile"
