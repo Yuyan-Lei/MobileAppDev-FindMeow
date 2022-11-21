4a5f74e740918b790b1407d062b5752fcafbb5ec
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-11-20 17:15:38 -0800

    feat: `HeartButton` changes color when clicking

--- components/pressable/HeartButton.jsx
+++ components/pressable/HeartButton.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 
-export function HeartButton({ onPress }) {
+export function HeartButton({
+  onPress,
+  isLiked,
+  selectedColor,
+  notSelectedColor,
+}) {
+  if (selectedColor === undefined) {
+    selectedColor = "orange";
+  }
+
+  if (notSelectedColor === undefined) {
+    notSelectedColor = "gray";
+  }
+
   return (
     <Pressable onPress={onPress}>
       <View style={{ padding: 8 }}>
-        <Text style={{ color: "orange", fontSize: 24, fontWeight: "600" }}>
+        <Text
+          style={{
+            color: isLiked ? selectedColor : notSelectedColor,
+            fontSize: 24,
+            fontWeight: "600",
+          }}
+        >
           ♡
         </Text>
       </View>
