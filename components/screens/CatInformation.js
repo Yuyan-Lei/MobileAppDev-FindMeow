63faa0e6e0abe1e286d279ae819c89aabfec64b0
CuichanWu <wu.cu@northeastern.edu>
2022-11-21 14:48:57 -0800

    style: check bottom drawer

--- components/screens/CatInformation.js
+++ components/screens/CatInformation.js
@@ -22,243 +22,265 @@ import {
   IconButton,
 } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import BottomDrawer from "react-native-bottom-drawer-view";
 
-const CatInformation = () => {
-  const [visible, setVisible] = useState(false);
-  const [value, setValue] = useState(0);
-  const { height, width } = useWindowDimensions();
-  const [revealed, setRevealed] = useState(false);
+// const CatInformation = () => {
+//   const [visible, setVisible] = useState(false);
+//   const [value, setValue] = useState(0);
+//   const { height, width } = useWindowDimensions();
+//   const [revealed, setRevealed] = useState(false);
 
-  return (
-    // <View style={styles.container}>
-    //   <ImageBackground
-    //     source={require("../pictures/kaka.jpg")}
-    //     resizeMode="cover"
-    //     style={styles.image}
-    //   >
-    //     <Button title="Cat info" buttonStyle={styles.button} />
+//   return (
+// <View style={styles.container}>
+//   <ImageBackground
+//     source={require("../pictures/kaka.jpg")}
+//     resizeMode="cover"
+//     style={styles.image}
+//   >
+//     <Button title="Cat info" buttonStyle={styles.button} />
 
-    //     <Overlay style={styles.card}>
-    //       <View style={{ position: "absolute", top: 12, left: 12 }}>
-    //         <View style={{ opacity: 0.5 }}>
-    //           <Pressable onPress={rootStackNavigateBack}>
-    //             <Feather name="arrow-left-circle" size={24} color="black" />
-    //           </Pressable>
-    //         </View>
-    //       </View>
+//     <Overlay style={styles.card}>
+//       <View style={{ position: "absolute", top: 12, left: 12 }}>
+//         <View style={{ opacity: 0.5 }}>
+//           <Pressable onPress={rootStackNavigateBack}>
+//             <Feather name="arrow-left-circle" size={24} color="black" />
+//           </Pressable>
+//         </View>
+//       </View>
 
-    //       {/* card format */}
-    //       <View
-    //         style={{
-    //           flexDirection: "row",
-    //           justifyContent: "space-evenly",
-    //           marginVertical: 20,
-    //         }}
-    //       >
-    //         <View style={styles.label}>
-    //           <View style={styles.tags}>
-    //             <Text style={{ color: Colors.gray }}>Gender</Text>
-    //             <Text>Female</Text>
-    //           </View>
-    //           <View style={styles.tags}>
-    //             <Text style={{ color: Colors.gray }}>Age</Text>
-    //             <Text>1 year</Text>
-    //           </View>
-    //           <View style={styles.tags}>
-    //             <Text style={{ color: Colors.gray }}>Breed</Text>
-    //             <Text>Unknown</Text>
-    //           </View>
-    //         </View>
-    //       </View>
+//       {/* card format */}
+//       <View
+//         style={{
+//           flexDirection: "row",
+//           justifyContent: "space-evenly",
+//           marginVertical: 20,
+//         }}
+//       >
+//         <View style={styles.label}>
+//           <View style={styles.tags}>
+//             <Text style={{ color: Colors.gray }}>Gender</Text>
+//             <Text>Female</Text>
+//           </View>
+//           <View style={styles.tags}>
+//             <Text style={{ color: Colors.gray }}>Age</Text>
+//             <Text>1 year</Text>
+//           </View>
+//           <View style={styles.tags}>
+//             <Text style={{ color: Colors.gray }}>Breed</Text>
+//             <Text>Unknown</Text>
+//           </View>
+//         </View>
+//       </View>
 
-    //       <View style={{ flexDirection: "row" }}>
-    //         <Text style={styles.textPrimary}>Kaka</Text>
-    //         <Text
-    //           style={{
-    //             textAlign: "right",
-    //             fontSize: 20,
-    //             fontWeight: "bold",
-    //             color: Colors.darkOrange,
-    //             marginLeft: "auto",
-    //           }}
-    //         >
-    //           $200
-    //         </Text>
-    //       </View>
+//       <View style={{ flexDirection: "row" }}>
+//         <Text style={styles.textPrimary}>Kaka</Text>
+//         <Text
+//           style={{
+//             textAlign: "right",
+//             fontSize: 20,
+//             fontWeight: "bold",
+//             color: Colors.darkOrange,
+//             marginLeft: "auto",
+//           }}
+//         >
+//           $200
+//         </Text>
+//       </View>
 
-    //       <View style={{ flexDirection: "row" }}>
-    //         <Ionicons
-    //           name="location-sharp"
-    //           size={24}
-    //           color={Colors.darkOrange}
-    //         />
-    //         <Text style={styles.textSecondary}>Santa Clara (0.8km)</Text>
-    //       </View>
+//       <View style={{ flexDirection: "row" }}>
+//         <Ionicons
+//           name="location-sharp"
+//           size={24}
+//           color={Colors.darkOrange}
+//         />
+//         <Text style={styles.textSecondary}>Santa Clara (0.8km)</Text>
+//       </View>
 
-    //       <Text style={styles.date}>Nov 17, 2022</Text>
-    //       <View style={styles.chipBox}>
-    //         <Chip title="Vaccinated" containerStyle={styles.chip} />
-    //         <Chip title="Vet Checked" containerStyle={styles.chip} />
-    //         <Chip title="Dewormed" containerStyle={styles.chip} />
-    //         <Text>Hi</Text>
-    //       </View>
+//       <Text style={styles.date}>Nov 17, 2022</Text>
+//       <View style={styles.chipBox}>
+//         <Chip title="Vaccinated" containerStyle={styles.chip} />
+//         <Chip title="Vet Checked" containerStyle={styles.chip} />
+//         <Chip title="Dewormed" containerStyle={styles.chip} />
+//         <Text>Hi</Text>
+//       </View>
 
-    //       <Text style={styles.contact}>Contact Info</Text>
-    //       <View style={{ flexDirection: "row" }}>
-    //         <View>
-    //           <Text>Angel Girls</Text>
-    //           <Text style={styles.date}>Cattery</Text>
-    //         </View>
-    //         <View style={styles.buttonView}>
-    //           <PhoneButton />
-    //           <MessageButton />
-    //         </View>
-    //       </View>
-    //     </Overlay>
-    //   </ImageBackground>
-    // </View>
+//       <Text style={styles.contact}>Contact Info</Text>
+//       <View style={{ flexDirection: "row" }}>
+//         <View>
+//           <Text>Angel Girls</Text>
+//           <Text style={styles.date}>Cattery</Text>
+//         </View>
+//         <View style={styles.buttonView}>
+//           <PhoneButton />
+//           <MessageButton />
+//         </View>
+//       </View>
+//     </Overlay>
+//   </ImageBackground>
+// </View>
 
-    // new version
-    // <View>
-    //   {/* <View> */}
-    //     <View style={{ height: 400, backgroundColor: "gray" }}>
-    //       <Image
-    //         source={require("../pictures/kaka.jpg")}
-    //         resizeMode="cover"
-    //         style={{ height: 430, width: width }}
-    //       ></Image>
-    //     </View>
-    //   </View>
+// new version
+// <View>
+//   {/* <View> */}
+//     <View style={{ height: 400, backgroundColor: "gray" }}>
+//       <Image
+//         source={require("../pictures/kaka.jpg")}
+//         resizeMode="cover"
+//         style={{ height: 430, width: width }}
+//       ></Image>
+//     </View>
+//   </View>
 
-    //   <View style={{ position: "absolute", top: 48, left: 12 }}>
-    //     <View style={{ opacity: 0.5 }}>
-    //       <Pressable onPress={rootStackNavigateBack}>
-    //         <Feather name="arrow-left-circle" size={24} color="white" />
-    //       </Pressable>
-    //     </View>
-    //   </View>
+//   <View style={{ position: "absolute", top: 48, left: 12 }}>
+//     <View style={{ opacity: 0.5 }}>
+//       <Pressable onPress={rootStackNavigateBack}>
+//         <Feather name="arrow-left-circle" size={24} color="white" />
+//       </Pressable>
+//     </View>
+//   </View>
 
-    // <View style={{ top: -80, width: width }}>
-    //   <View style={{ height: 24 }} />
-    //   <View
-    //     style={{ padding: 24, backgroundColor: "white", borderRadius: 35 }}
-    //   >
-    //     <Text style={{ color: "orange", fontWeight: "500" }}>About</Text>
-    //     <Text>hello world</Text>
-    //     <View style={{ flexDirection: "row" }}>
-    //       <Text style={{ fontWeight: "500", color: Colors.black }}>
-    //         Phone:{" "}
-    //       </Text>
-    //       <Text>111</Text>
-    //     </View>
-    //     <View style={{ flexDirection: "row" }}>
-    //       <Text style={{ fontWeight: "500", color: Colors.black }}>
-    //         Website:{" "}
-    //       </Text>
-    //       <Text>http://xxx</Text>
-    //     </View>
-    //   </View>
-    // </View>
-    // </View>
+// <View style={{ top: -80, width: width }}>
+//   <View style={{ height: 24 }} />
+//   <View
+//     style={{ padding: 24, backgroundColor: "white", borderRadius: 35 }}
+//   >
+//     <Text style={{ color: "orange", fontWeight: "500" }}>About</Text>
+//     <Text>hello world</Text>
+//     <View style={{ flexDirection: "row" }}>
+//       <Text style={{ fontWeight: "500", color: Colors.black }}>
+//         Phone:{" "}
+//       </Text>
+//       <Text>111</Text>
+//     </View>
+//     <View style={{ flexDirection: "row" }}>
+//       <Text style={{ fontWeight: "500", color: Colors.black }}>
+//         Website:{" "}
+//       </Text>
+//       <Text>http://xxx</Text>
+//     </View>
+//   </View>
+// </View>
+// </View>
 
-    <Backdrop
-      revealed={revealed}
-      header={
-        <AppBar
-          style={{
-            marginTop: 450,
-            alignSelf: "flex-end",
-            // backgroundColor: "black",
-          }}
-          // title="See More"
-          transparent
-          leading={(props) => (
-            <IconButton
-              icon={(props) => (
-                <Icon name={revealed ? "close" : "menu"} {...props} />
-              )}
-              onPress={() => setRevealed((prevState) => !prevState)}
-              {...props}
-            />
-          )}
-        />
-      }
-      style={{ marginTop: -30 }}
-      headerContainerStyle={{ backgroundColor: "gray" }}
-      subheaderContainerStyle={{ backgroundColor: "black" }}
-      // frontLayerContainerStyle={{ backgroundColor: "yellow" }}
-      backLayerContainerStyle={{ backgroundColor: "green" }}
-      backLayer={<View style={{ height: 200 }} />}
-    >
-      <BackdropSubheader
-        // title="Subheader Hello World"
-        style={{ textAlign: "center" }}
-      />
-      {/* <Text>hihihihi</Text> */}
-      <View style={{ marginHorizontal: 20 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <View style={styles.label}>
-            <View style={styles.tags}>
-              <Text style={{ color: Colors.gray }}>Gender</Text>
-              <Text>Female</Text>
-            </View>
-            <View style={styles.tags}>
-              <Text style={{ color: Colors.gray }}>Age</Text>
-              <Text>1 year</Text>
-            </View>
-            <View style={styles.tags}>
-              <Text style={{ color: Colors.gray }}>Breed</Text>
-              <Text>Unknown</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.textPrimary}>Kaka</Text>
-          <Text
-            style={{
-              textAlign: "right",
-              fontSize: 20,
-              fontWeight: "bold",
-              color: Colors.darkOrange,
-              marginLeft: "auto",
-            }}
-          >
-            $200
-          </Text>
-        </View>
+// second version
+// <Backdrop
+//   revealed={revealed}
+//   header={
+//     <AppBar
+//       style={{
+//         marginTop: 450,
+//         alignSelf: "flex-end",
+//         // backgroundColor: "black",
+//       }}
+//       // title="See More"
+//       transparent
+//       leading={(props) => (
+//         <IconButton
+//           icon={(props) => (
+//             <Icon name={revealed ? "close" : "menu"} {...props} />
+//           )}
+//           onPress={() => setRevealed((prevState) => !prevState)}
+//           {...props}
+//         />
+//       )}
+//     />
+//   }
+//   style={{ marginTop: -30 }}
+//   headerContainerStyle={{ backgroundColor: "gray" }}
+//   subheaderContainerStyle={{ backgroundColor: "black" }}
+//   // frontLayerContainerStyle={{ backgroundColor: "yellow" }}
+//   backLayerContainerStyle={{ backgroundColor: "green" }}
+//   backLayer={<View style={{ height: 200 }} />}
+// >
+//   <BackdropSubheader
+//     // title="Subheader Hello World"
+//     style={{ textAlign: "center" }}
+//   />
+//   {/* <Text>hihihihi</Text> */}
+//   <View style={{ marginHorizontal: 20 }}>
+//     <View
+//       style={{
+//         flexDirection: "row",
+//         justifyContent: "space-evenly",
+//       }}
+//     >
+//       <View style={styles.label}>
+//         <View style={styles.tags}>
+//           <Text style={{ color: Colors.gray }}>Gender</Text>
+//           <Text>Female</Text>
+//         </View>
+//         <View style={styles.tags}>
+//           <Text style={{ color: Colors.gray }}>Age</Text>
+//           <Text>1 year</Text>
+//         </View>
+//         <View style={styles.tags}>
+//           <Text style={{ color: Colors.gray }}>Breed</Text>
+//           <Text>Unknown</Text>
+//         </View>
+//       </View>
+//     </View>
+//     <View style={{ flexDirection: "row" }}>
+//       <Text style={styles.textPrimary}>Kaka</Text>
+//       <Text
+//         style={{
+//           textAlign: "right",
+//           fontSize: 20,
+//           fontWeight: "bold",
+//           color: Colors.darkOrange,
+//           marginLeft: "auto",
+//         }}
+//       >
+//         $200
+//       </Text>
+//     </View>
 
-        <View style={{ flexDirection: "row" }}>
-          <Ionicons name="location-sharp" size={24} color={Colors.darkOrange} />
-          <Text style={styles.textSecondary}>Santa Clara (0.8km)</Text>
-        </View>
+//     <View style={{ flexDirection: "row" }}>
+//       <Ionicons name="location-sharp" size={24} color={Colors.darkOrange} />
+//       <Text style={styles.textSecondary}>Santa Clara (0.8km)</Text>
+//     </View>
 
-        <Text style={styles.date}>Nov 17, 2022</Text>
-        <View style={styles.chipBox}>
-          <Chip title="Vaccinated" containerStyle={styles.chip} />
-          <Chip title="Vet Checked" containerStyle={styles.chip} />
-          <Chip title="Dewormed" containerStyle={styles.chip} />
-        </View>
+//     <Text style={styles.date}>Nov 17, 2022</Text>
+//     <View style={styles.chipBox}>
+//       <Chip title="Vaccinated" containerStyle={styles.chip} />
+//       <Chip title="Vet Checked" containerStyle={styles.chip} />
+//       <Chip title="Dewormed" containerStyle={styles.chip} />
+//     </View>
 
-        <Text style={styles.contact}>Contact Info</Text>
-        <View style={{ flexDirection: "row" }}>
-          <View>
-            <Text>Angel Girls</Text>
-            <Text style={styles.date}>Cattery</Text>
-          </View>
-          <View style={styles.buttonView}>
-            <PhoneButton />
-            <MessageButton />
-          </View>
-        </View>
+//     <Text style={styles.contact}>Contact Info</Text>
+//     <View style={{ flexDirection: "row" }}>
+//       <View>
+//         <Text>Angel Girls</Text>
+//         <Text style={styles.date}>Cattery</Text>
+//       </View>
+//       <View style={styles.buttonView}>
+//         <PhoneButton />
+//         <MessageButton />
+//       </View>
+//     </View>
+//   </View>
+// </Backdrop>
+
+//   );
+// };
+
+const IMAGE_HEIGHT = 49;
+// const HEADER_HEIGHT = 60;
+export default class CatInformation extends React.Component {
+  renderContent = () => {
+    return (
+      <View>
+        <Text>hello world</Text>
       </View>
-    </Backdrop>
-  );
-};
+    );
+  };
+  render() {
+    return (
+      <BottomDrawer containerHeight={400} offset={IMAGE_HEIGHT}>
+        {this.renderContent()}
+      </BottomDrawer>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   button: {
@@ -328,5 +350,3 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
 });
-
-export default CatInformation;
