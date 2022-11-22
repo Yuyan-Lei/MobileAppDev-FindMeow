a43f4dc113a6c8c00c50145a76c765b94c8a0a7d
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-11-21 23:24:16 -0800

    fix: type

--- components/cards/BreederCard.jsx
+++ components/cards/BreederCard.jsx
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   shadowView: {
-    shadowColor: Platform.OS === 'ios' ? Colors.Purple : Colors.shadowWhiteAndroid,
+    shadowColor: Platform.OS === 'ios' ? Colors.purple : Colors.shadowWhiteAndroid,
     shadowOffset: {
       width: 40,
       height: 80,
