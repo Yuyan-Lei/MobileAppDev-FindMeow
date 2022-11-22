6d8ec3a6ecf96428490991c2db7f3fcfce69d0ab
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-11-21 21:51:34 -0800

    test: Shadow

--- components/cards/BreederCard.jsx
+++ components/cards/BreederCard.jsx
@@ -1,31 +1,24 @@
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { HeartButton } from "../pressable/HeartButton";
 import { rootStackNavigate } from "../RootNavigation";
 import { Colors } from "../styles/Colors";
 import { LocationText } from "../texts/LocationText";
+// import DropShadow from "react-native-drop-shadow";
+import { Shadow } from "react-native-shadow-2";
 
 export function BreederCard({ breeder }) {
   return (
     <View>
+      <Shadow
+        distance={5}
+        startColor={"#00000010"}
+        containerViewStyle={{ marginVertical: 20 }}
+        radius={8}
+      ></Shadow>
       <Pressable onPress={() => rootStackNavigate("CatteryProfile")}>
-        <View
-          style={{
-            flexDirection: "row",
-            marginVertical: 16,
-            backgroundColor: "white",
-            borderRadius: 20,
-          }}
-        >
-          <View
-            style={{
-              height: 80,
-              width: 80,
-              backgroundColor: "gray",
-              marginHorizontal: 20,
-              borderRadius: 16,
-            }}
-          ></View>
+        <View style={styles.cardView}>
+          <View style={styles.imageView}></View>
 
           <View style={{ width: 360 }}>
             <Text style={{ fontWeight: "bold", color: Colors.black }}>
@@ -48,3 +41,19 @@ export function BreederCard({ breeder }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imageView: {
+    height: 80,
+    width: 80,
+    backgroundColor: "gray",
+    marginHorizontal: 20,
+    borderRadius: 16,
+  },
+  cardView: {
+    flexDirection: "row",
+    marginVertical: 16,
+    backgroundColor: "white",
+    borderRadius: 20,
+  },
+});
