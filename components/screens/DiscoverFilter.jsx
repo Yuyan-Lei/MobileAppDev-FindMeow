7cd35d899d6d3b8aff1d97e84fe2faaa667ab1e4
Yuyan Lei <lei.yuy@northeastern.edu>
2022-11-23 14:21:06 -0800

    fix: fix location bugs in the star list screen

--- components/screens/DiscoverFilter.jsx
+++ components/screens/DiscoverFilter.jsx
@@ -693,42 +693,6 @@ const DiscoverFilter = ({
         defaultOption={{ key: selectedState, value: selectedState }}
       />
 
-      {/* <View style={{ flexDirection: "row" }}>
-        <CheckBox
-          center
-          title="Vaccinated"
-          checked={check1}
-          onPress={() => setCheck1(!check1)}
-        />
-        <CheckBox
-          center
-          title="Vet Checked"
-          checked={check2}
-          onPress={() => setCheck2(!check2)}
-        />
-        <CheckBox
-          center
-          title="Dewormed"
-          checked={check3}
-          onPress={() => setCheck3(!check3)}
-        />
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <CheckBox
-          center
-          title="Ready to go home"
-          checked={check4}
-          onPress={() => setCheck4(!check4)}
-        />
-        <CheckBox
-          center
-          title="Neutered / Spayed"
-          checked={check5}
-          onPress={() => setCheck5(!check5)}
-        />
-      </View> */}
-
 
       <View style={styles.submitButtonContainer}>
         <Pressable
