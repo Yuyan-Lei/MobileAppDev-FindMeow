5d1dab10249de02af71862c8991ae44c84b522ca
CuichanWu <wu.cu@northeastern.edu>
2022-11-28 22:27:37 -0800

    fix: syntax error

--- components/screens/ProfileCatteryPage.jsx
+++ components/screens/ProfileCatteryPage.jsx
@@ -90,8 +90,7 @@ function MainScreen({ route, navigation }) {
       cattery: cat.Cattery,
       photo: cat.Picture,
       breed: cat.breed,
-    }
-
+    };
   };
 
   const onUpdateCattery = () => {
@@ -172,14 +171,14 @@ function MainScreen({ route, navigation }) {
               renderItem={({ item, index }) => (
                 <CatCard
                   cat={buildCatItem(item)}
-
-                  navigation={navigation} 
+                  navigation={navigation}
                   hideLocation
-                  showBreed />}
+                  showBreed
+                />
+              )}
               numColumns={2}
-              ListFooterComponent={<View style={{height: 1550}}/>}
-              />
-
+              ListFooterComponent={<View style={{ height: 1550 }} />}
+            />
           </View>
         </View>
 
