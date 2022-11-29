7938f8f4e19b6cc77de096dfef7307cee23aa221
Yuyan-Lei <82356933+Yuyan-Lei@users.noreply.github.com>
2022-11-28 21:45:27 -0800

    style: restyle the `CatInformationScreen`

--- components/screens/CatInformation.js
+++ components/screens/CatInformation.js
@@ -331,97 +331,97 @@ import { Client } from "@googlemaps/google-maps-services-js";
 // import { Chip } from "../pressable/Chip";
 
 export default function CatInformation({ route, navigation }) {
-  function RenderContent() {
-    return (
-      <View style={{ marginHorizontal: 15 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            marginVertical: 20,
-          }}
-        >
-          <View style={styles.label}>
-            <View style={styles.tags}>
-              <Text style={styles.tagText}>Gender</Text>
-              <Text style={styles.tagInfoText}>{cat.Gender}</Text>
-            </View>
-            <View style={styles.tags}>
-              <Text style={styles.tagText}>Age</Text>
-              <Text style={styles.tagInfoText}>
-                {cat.month} {cat.month === 1 ? "month" : "months"}
-              </Text>
-            </View>
-            <View style={styles.tags}>
-              <Text style={styles.tagText}>Breed</Text>
-              <Text style={styles.tagInfoText}>{cat.Breed}</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.textPrimary}>{cat.Name}</Text>
-          <Text
-            style={{
-              textAlign: "right",
-              fontSize: 20,
-              fontWeight: "bold",
-              color: Colors.darkOrange,
-              marginLeft: "auto",
-              marginVertical: 20,
-            }}
-          >
-            ${cat.Price}
-          </Text>
-        </View>
-        {/* TODO: CATTERY LOCATION */}
-        <View style={{ flexDirection: "row" }}>
-          <Ionicons name="location-sharp" size={24} color={Colors.darkOrange} />
-          <Text style={styles.textSecondary}>
-            {cattery.address} ({distance})
-          </Text>
-        </View>
-        <Text style={styles.date}>{cat.Birthday}</Text>
-        <View style={styles.chipBox}>
-          {cat.Tags ? (
-            cat.Tags.map((tag, index) => (
-              <Chip
-                title={tag}
-                key={index}
-                containerStyle={styles.chip}
-                color="#F59156"
-                titleStyle={"Poppins"}
-              />
-            ))
-          ) : (
-            <></>
-          )}
-        </View>
+  // function RenderContent() {
+  //   return (
+  //     <View style={{ marginHorizontal: 15 }}>
+  //       <View
+  //         style={{
+  //           flexDirection: "row",
+  //           justifyContent: "space-evenly",
+  //           marginVertical: 20,
+  //         }}
+  //       >
+  //         <View style={styles.label}>
+  //           <View style={styles.tags}>
+  //             <Text style={styles.tagText}>Gender</Text>
+  //             <Text style={styles.tagInfoText}>{cat.Gender}</Text>
+  //           </View>
+  //           <View style={styles.tags}>
+  //             <Text style={styles.tagText}>Age</Text>
+  //             <Text style={styles.tagInfoText}>
+  //               {cat.month} {cat.month === 1 ? "month" : "months"}
+  //             </Text>
+  //           </View>
+  //           <View style={styles.tags}>
+  //             <Text style={styles.tagText}>Breed</Text>
+  //             <Text style={styles.tagInfoText}>{cat.Breed}</Text>
+  //           </View>
+  //         </View>
+  //       </View>
+  //       <View style={{ flexDirection: "row" }}>
+  //         <Text style={styles.catNameText}>{cat.Name}</Text>
+  //         <Text
+  //           style={{
+  //             textAlign: "right",
+  //             fontSize: 20,
+  //             fontWeight: "bold",
+  //             color: Colors.darkOrange,
+  //             marginLeft: "auto",
+  //             marginVertical: 20,
+  //           }}
+  //         >
+  //           ${cat.Price}
+  //         </Text>
+  //       </View>
+  //       {/* TODO: CATTERY LOCATION */}
+  //       <View style={{ flexDirection: "row" }}>
+  //         <Ionicons name="location-sharp" size={24} color={Colors.darkOrange} />
+  //         <Text style={styles.textSecondary}>
+  //           {cattery.address} ({distance})
+  //         </Text>
+  //       </View>
+  //       <Text style={styles.date}>{cat.Birthday}</Text>
+  //       <View style={styles.chipBox}>
+  //         {cat.Tags ? (
+  //           cat.Tags.map((tag, index) => (
+  //             <Chip
+  //               title={tag}
+  //               key={index}
+  //               containerStyle={styles.chip}
+  //               color="#F59156"
+  //               titleStyle={"Poppins"}
+  //             />
+  //           ))
+  //         ) : (
+  //           <></>
+  //         )}
+  //       </View>
 
-        {/* contact info label */}
-        <View style={styles.contactLabel}>
-          <Text style={styles.contact}>Contact Info</Text>
-          <View style={{ flexDirection: "row" }}>
-            <View>
-              <Text style={styles.text}>{cattery.catteryName}</Text>
-              <Text style={styles.date}>Cattery</Text>
-            </View>
-            <View style={{ width: 200 }}></View>
-            <View style={styles.buttonView}>
-              <PhoneButton />
-              <MessageButton />
-            </View>
-          </View>
-        </View>
-        {/* contact info label end */}
-        <View style={styles.detailLabel}>
-          <Text style={styles.contact}>Details</Text>
-          <View>
-            <Text>{cat.Description}</Text>
-          </View>
-        </View>
-      </View>
-    );
-  }
+  //       {/* contact info label */}
+  //       <View style={styles.contactLabel}>
+  //         <Text style={styles.contact}>Contact Info</Text>
+  //         <View style={{ flexDirection: "row" }}>
+  //           <View>
+  //             <Text style={styles.text}>{cattery.catteryName}</Text>
+  //             <Text style={styles.date}>Cattery</Text>
+  //           </View>
+  //           <View style={{ width: 200 }}></View>
+  //           <View style={styles.buttonView}>
+  //             <PhoneButton />
+  //             <MessageButton />
+  //           </View>
+  //         </View>
+  //       </View>
+  //       {/* contact info label end */}
+  //       <View style={styles.detailLabel}>
+  //         <Text style={styles.contact}>Details</Text>
+  //         <View>
+  //           <Text style={styles.descriptionText}>{cat.Description}</Text>
+  //         </View>
+  //       </View>
+  //     </View>
+  //   );
+  // }
 
   const catId = route.params.catId;
   const [cat, setCat] = useState({});
@@ -491,12 +491,16 @@ export default function CatInformation({ route, navigation }) {
 
   return (
     <ScrollView>
+
       <View>
+
+        {/* Background Image */}
         <Image
           source={{ uri: cat.Picture }}
           resizeMode="cover"
           style={{ height: 450, width: 500 }}
         ></Image>
+
         {/* START: goBack button and heart button */}
         <View style={styles.floatingView}>
           <HeartButton
@@ -506,6 +510,7 @@ export default function CatInformation({ route, navigation }) {
           />
         </View>
 
+        {/* Back Button*/}
         <View
           style={{
             opacity: 0.5,
@@ -522,7 +527,13 @@ export default function CatInformation({ route, navigation }) {
           </Pressable>
         </View>
         {/* END: goBack button and heart button */}
-        <View style={{ marginHorizontal: 15 }}>
+
+
+        {/* Main Contents */}
+        <View style={{ 
+          marginHorizontal: 15,
+          borderRadius: 40,
+         }}>
           <View
             style={{
               flexDirection: "row",
@@ -532,33 +543,26 @@ export default function CatInformation({ route, navigation }) {
           >
             <View style={styles.label}>
               <View style={styles.tags}>
-                <Text style={styles.tagText}>Gender</Text>
-                <Text style={styles.tagInfoText}>{cat.Gender}</Text>
+                <Text style={styles.tagTitleText}>Gender</Text>
+                <Text style={styles.tagContentText}>{cat.Gender}</Text>
               </View>
               <View style={styles.tags}>
-                <Text style={styles.tagText}>Age</Text>
-                <Text style={styles.tagInfoText}>
+                <Text style={styles.tagTitleText}>Age</Text>
+                <Text style={styles.tagContentText}>
                   {cat.month} {cat.month === 1 ? "month" : "months"}
                 </Text>
               </View>
               <View style={styles.tags}>
-                <Text style={styles.tagText}>Breed</Text>
-                <Text style={styles.tagInfoText}>{cat.Breed}</Text>
+                <Text style={styles.tagTitleText}>Breed</Text>
+                <Text style={styles.tagContentText}>{cat.Breed}</Text>
               </View>
             </View>
           </View>
+          
           <View style={{ flexDirection: "row" }}>
-            <Text style={styles.textPrimary}>{cat.Name}</Text>
+            <Text style={styles.catNameText}>{cat.Name}</Text>
             <Text
-              style={{
-                textAlign: "right",
-                fontSize: 20,
-                fontWeight: "bold",
-                color: Colors.darkOrange,
-                marginLeft: "auto",
-                marginVertical: 20,
-                fontFamily: "Poppins",
-              }}
+              style={styles.priceText}
             >
               ${cat.Price}
             </Text>
@@ -570,10 +574,10 @@ export default function CatInformation({ route, navigation }) {
               size={24}
               color={Colors.darkOrange}
             />
-            <Text style={styles.textSecondary}>{cattery.address}</Text>
+            <Text style={styles.addressText}>{cattery.address}</Text>
           </View>
           {/* <Text style={styles.date}>{cat.Birthday}</Text> */}
-          <Text style={styles.date}>Posted date: {cat.Birthday}</Text>
+          <Text style={styles.PostDateText}>Posted in {cat.Birthday}</Text>
           <View style={styles.chipBox}>
             {cat.Tags ? (
               cat.Tags.map((tag, index) => (
@@ -590,12 +594,12 @@ export default function CatInformation({ route, navigation }) {
           </View>
 
           {/* contact info label */}
-          <View style={styles.contactLabel}>
-            <Text style={styles.contact}>Contact Info</Text>
+          <View style={styles.contactLabelContainer}>
+            <Text style={styles.contactText}>Contact Info</Text>
             <View style={{ flexDirection: "row" }}>
               <View>
-                <Text>{cattery.catteryName}</Text>
-                <Text style={styles.date}>Cattery</Text>
+                <Text style={styles.catteryNameText}>{cattery.catteryName}</Text>
+                <Text style={styles.catteryLabelText}>Cattery</Text>
               </View>
               <View style={{ width: 200 }}></View>
               <View style={styles.buttonView}>
@@ -606,10 +610,10 @@ export default function CatInformation({ route, navigation }) {
           </View>
           {/* contact info label end */}
           <View style={styles.detailLabel}>
-            <Text style={styles.contact}>Details</Text>
+            <Text style={styles.detailTitleText}>Details</Text>
 
             <View>
-              <Text>{cat.Description}</Text>
+              <Text style={styles.descriptionText}>{cat.Description}</Text>
             </View>
           </View>
         </View>
@@ -623,23 +627,19 @@ const styles = StyleSheet.create({
     height: 450,
     width: 500,
   },
-  tagInfoText: {
-    fontWeight: "bold",
-    fontSize: 16,
-    fontFamily: "Poppins",
-  },
-  button: {
-    // margin: 10,
-    marginTop: 1000,
-    width: 100,
-    alignSelf: "flex-end",
-  },
-  card: {
-    width: "100%",
-    position: "absolute",
-    bottom: 0,
-  },
+  // button: {
+  //   // margin: 10,
+  //   marginTop: 1000,
+  //   width: 100,
+  //   alignSelf: "flex-end",
+  // },
+  // card: {
+  //   width: "100%",
+  //   position: "absolute",
+  //   bottom: 0,
+  // },
   label: {
+    marginTop: 10,
     alignItems: "center",
     flexDirection: "row",
   },
@@ -650,53 +650,33 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 12,
   },
-  tagText: {
-    color: Colors.gray,
-    padding: 5,
-    width: 80,
-    textAlign: "center",
-    fontFamily: "Poppins",
-  },
-  contactLabel: {
-    backgroundColor: "white",
-    alignItems: "center",
-    padding: 8,
-    borderRadius: 12,
-    marginTop: 10,
-  },
-  detailLabel: {
-    textAlign: "left",
-    marginTop: 10,
-    paddingVertical: 8,
-    fontFamily: "Poppins",
-    marginBottom: 50,
-  },
-  textPrimary: {
-    textAlign: "left",
-    fontSize: 28,
-    fontWeight: "bold",
-    marginVertical: 20,
-    fontFamily: "Poppins",
-  },
-  textSecondary: {
-    marginBottom: 10,
-    textAlign: "left",
-    fontSize: 14,
-    fontFamily: "Poppins",
-    // marginLeft: 6,
-  },
+  // tagText: {
+  //   color: Colors.gray,
+  //   padding: 5,
+  //   width: 80,
+  //   textAlign: "center",
+  //   fontFamily: "Poppins",
+  // },
+
+  // textPrimary: {
+  //   textAlign: "left",
+  //   fontSize: 28,
+  //   fontWeight: "bold",
+  //   marginVertical: 20,
+  //   fontFamily: "Poppins",
+  // },
+  // textSecondary: {
+  //   marginBottom: 10,
+  //   textAlign: "left",
+  //   fontSize: 14,
+  //   fontFamily: "Poppins",
+  //   // marginLeft: 6,
+  // },
   chip: {
     marginVertical: 8,
     // marginHorizontal: 6,
     marginRight: 10,
     height: 35,
-  },
-  contact: {
-    marginBottom: 20,
-    marginRight: "auto",
-    marginTop: 5,
-    fontSize: 16,
-    fontFamily: "PoppinsSemiBold",
   },
   buttonView: {
     flexDirection: "row",
@@ -716,11 +696,11 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     // justifyContent: "space-evenly",
   },
-  date: {
-    color: Colors.gray,
-    marginVertical: 8,
-    fontFamily: "Poppins",
-  },
+  // date: {
+  //   color: Colors.gray,
+  //   marginVertical: 8,
+  //   fontFamily: "Poppins",
+  // },
   text: {
     marginTop: 20,
     marginBottom: 10,
@@ -730,5 +710,94 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 40,
     right: 32,
+  },
+
+  tagTitleText: {
+    fontSize: 13,
+    color: 'rgba(46, 37, 37, 0.5)',
+    fontFamily: "PoppinsRegular",
+    marginVertical: 5,
+  },
+  tagContentText: {
+    fontSize: 15,
+    color: '#2E2525',
+    fontFamily: "PoppinsSemiBold",
+    marginBottom: 5,
+  },
+
+  catNameText: {
+    fontSize: 30,
+    color: '#2E2525',
+    fontFamily: "PoppinsBold",
+    marginVertical: 20,
+  },
+  priceText: {
+    fontSize: 25,
+    color: '#F6AC3D',
+    fontFamily: "PoppinsMedium",
+    textAlign: "right",
+    marginLeft: "auto",
+    marginVertical: 20,
+  },
+
+  addressText: {
+    fontSize: 16,
+    color: '#2E2525',
+    fontFamily: "PoppinsMedium",
+    marginLeft: 5,
+  },
+  PostDateText : {
+    fontSize: 15,
+    color: 'rgba(46, 37, 37, 0.67)',
+    fontFamily: "PoppinsLight",
+    marginTop: 10,
+    marginBottom: 15,
+  },
+
+  detailLabel: {
+    textAlign: "left",
+    marginTop: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 5,
+    fontFamily: "Poppins",
+    marginBottom: 50,
+  },
+  detailTitleText: {
+    marginBottom: 15,
+    marginTop: 10,
+    fontSize: 16,
+    fontFamily: "PoppinsSemiBold",
+  },
+  descriptionText: {
+    fontSize: 15,
+    color: 'rgba(46, 37, 37, 0.76)',
+    fontFamily: "PoppinsRegular",
+  },
+  
+  contactLabelContainer: {
+    backgroundColor: "white",
+    alignItems: "center",
+    padding: 8,
+    borderRadius: 15,
+    marginTop: 20,
+  },
+  contactText: {
+    marginBottom: 15,
+    marginRight: "auto",
+    marginTop: 10,
+    fontSize: 16,
+    fontFamily: "PoppinsSemiBold",
+  },
+  catteryNameText: {
+    fontSize: 14,
+    color: '#2E2525',
+    fontFamily: "PoppinsMedium",
+  },
+  catteryLabelText: {
+    fontSize: 12,
+    color: 'rgba(46, 37, 37, 0.63)',
+    fontFamily: "PoppinsMedium",
+    marginTop: 5,
+    marginBottom: 15,
   },
 });
