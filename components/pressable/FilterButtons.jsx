b55c6c3faa95a61dd8456285768de7c979cde191
Yuyan Lei <lei.yuy@northeastern.edu>
2022-11-29 22:07:14 -0800

    feat: restyle the filter button component

--- components/pressable/FilterButtons.jsx
+++ components/pressable/FilterButtons.jsx
@@ -8,6 +8,7 @@ export function FilterButtons({ selectedIndex, setSelectedIndex, buttons }) {
     <View style={styles.container}>
       <View style={{ flex: 1 }}>
         <ButtonGroup
+          buttonStyle={styles.unselectedButtonStyle}
           buttons={buttons}
           selectedIndex={selectedIndex}
           onPress={(value) => {
@@ -30,15 +31,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     margin: 12,
   },
-  buttonContainerStyle: { height: 40, shadowRadius: 10, borderRadius: 10 },
+  buttonContainerStyle: { 
+    height: 40,
+    shadowRadius: 10, 
+    borderRadius: 10,
+    color: "white"
+  },
+
+  unselectedButtonStyle: {
+    backgroundColor: "#F9F9F9",
+    borderRadius: 10,
+    marginHorizontal: 3,
+  },
   selectedButtonStyle: {
     backgroundColor: "#FFB801",
+    borderRadius: 10,
+    marginHorizontal: 5,
   },
+
   buttonTextStyle: {
     fontSize: 14,
-    fontFamily: "Poppins",
+    fontFamily: "PoppinsMedium",
     color: "rgba(46, 37, 37, 0.43)",
     fontWeight: "500",
   },
-  selectedTextStyle: { color: "white" },
+  selectedTextStyle: { 
+    color: "white",
+    fontFamily: "PoppinsSemiBold",
+    fontSize: 15
+   },
 });
