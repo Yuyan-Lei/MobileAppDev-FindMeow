c9b506db386a104d6e7c82014e9b4a952c7f4dd2
CuichanWu <wu.cu@northeastern.edu>
2022-12-03 23:06:18 -0800

    feat: goBack button works

---
+++ components/pressable/CatteryMarker.js
@@ -0,0 +1,84 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  doc,
+  getDoc,
+  onSnapshot,
+} from "firebase/firestore";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+  Pressable,
+  Alert,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import MapView, {
+  PROVIDER_GOOGLE,
+  Marker,
+  Callout,
+  Overlay,
+  OverlayComponent,
+} from "react-native-maps";
+import { Foundation } from "@expo/vector-icons";
+import { Colors } from "../styles/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { CatCard_map } from "../cards/CatCard_map";
+import CatInformation from "../screens/CatInformation";
+import { db } from "../../firebaseUtils/firebase-setup";
+import {
+  calculateDistance,
+  getAllCatteries,
+  getUserLocation,
+} from "../../firebaseUtils/user";
+
+export function CatteryMarker() {
+  const { height, width } = useWindowDimensions();
+  const [showCatCard, setShowCatCard] = useState(false);
+  const showCatCardHandler = () => {
+    setShowCatCard(!showCatCard);
+  };
+
+  return (
+    <View>
+      <Marker
+        coordinate={{ latitude: 37.3387, longitude: -121.8853 }}
+        onPress={showCatCardHandler}
+      >
+        <View>
+          {/* <Pressable> */}
+          <Foundation name="marker" size={40} color={Colors.orangeText} />
+          {/* </Pressable> */}
+        </View>
+        {/* <Callout>
+            <View style={{ width: 100, height: 100, backgroundColor: "white" }}>
+              <Text>Hi Hi hi</Text>
+            </View>
+          </Callout> */}
+      </Marker>
+
+      {showCatCard == true ? (
+        <View style={{ width: width + 20, alignItems: "center" }}>
+          <View
+            style={{
+              height: 80,
+              backgroundColor: "transparent",
+              position: "absolute",
+              top: height - 170,
+            }}
+          >
+            <CatCard_map></CatCard_map>
+          </View>
+        </View>
+      ) : (
+        <View />
+      )}
+    </View>
+  );
+}
--- components/screens/MapPage.js
+++ components/screens/MapPage.js
@@ -37,6 +37,7 @@ import {
   getAllCatteries,
   getUserLocation,
 } from "../../firebaseUtils/user";
+import { CatteryMarker } from "../pressable/CatteryMarker";
 
 export default function MapPage({ route, navigation }) {
   const { height, width } = useWindowDimensions();
@@ -48,114 +49,7 @@ export default function MapPage({ route, navigation }) {
   // const [distance, setDistance] = useState("Distance Loading");
   // const [likeCats, setLikeCats] = useState([]);
   // const [allowEdit, setAllowEdit] = useState(false);
-  // const [data, setData] = useState([]);
-
-  // const buttonHandler = () => {
-  //   Alert.alert("Alert Title", "My Alert Msg", [
-  //     {
-  //       text: "Cancel",
-  //       onPress: () => console.log("Cancel Pressed"),
-  //       style: "cancel",
-  //     },
-  //     { text: "OK", onPress: () => console.log("OK Pressed") },
-  //   ]);
-  // };
-
-  // /* Set user location. */
-  // useEffect(() => {
-  //   (async () => {
-  //     let location = await getUserLocation();
-  //     setLocation(location);
-  //   })();
-  // }, []);
-
-  // /* Allow edit if user is the cat owner. */
-  // useEffect(() => {
-  //   if (cat && cat.Cattery) {
-  //     setAllowEdit(cat.Cattery === getCurrentUserEmail());
-  //   }
-  // }, [cat]);
-
-  // /* Calculate distance to the cat if both user location and cattery location are provided. */
-  // useEffect(() => {
-  //   if (cattery && cattery.placeId && location) {
-  //     const googleMapClient = new Client({});
-  //     googleMapClient
-  //       .placeDetails({
-  //         params: {
-  //           place_id: cattery.placeId,
-  //           key: REACT_APP_GOOGLE_MAP_APP_KEY,
-  //         },
-  //       })
-  //       .then((resp) => {
-  //         setDistance(
-  //           calculateDistance(location, resp.data.result.geometry.location) +
-  //             "mi"
-  //         );
-  //       });
-  //   }
-  // }, [cattery, location]);
-
-  // useEffect(() => {
-  //   const unSubscribe = onSnapshot(doc(db, "Cats", catId), async (catEntry) => {
-  //     const catData = catEntry.data();
-  //     const birthday = new Date(catData.Birthday);
-  //     const now = new Date();
-
-  //     let age =
-  //       now.getMonth() -
-  //       birthday.getMonth() +
-  //       12 * (now.getFullYear() - birthday.getFullYear());
-  //     // age cannot be negative
-  //     if (age === undefined || isNaN(age) || age < 0) {
-  //       age = 0;
-  //     }
-
-  //     /* Group cat object */
-  //     setCat({
-  //       ...catData,
-  //       id: catEntry.id,
-  //       month: age,
-  //     });
-
-  //     /* Get cattery */
-  //     const catteryRef = doc(db, "Users", catData.Cattery);
-  //     const catterySnap = await getDoc(catteryRef);
-
-  //     setCattery(catterySnap.data());
-  //   });
-
-  //   return () => unSubscribe();
-  // }, []);
-
-  // useEffect(() => {
-  //   const unSubscribe = onSnapshot(
-  //     doc(db, "Users", getCurrentUserEmail()),
-  //     (snapshot) => {
-  //       const likeCats = snapshot.data().likeCats;
-  //       setLikeCats(likeCats);
-  //     }
-  //   );
-
-  //   return () => unSubscribe();
-  // }, []);
-
-  // const onClickLikeButton = () => {
-  //   if (!likeCats.includes(cat.id)) {
-  //     userLikeACat(cat.id);
-  //   } else {
-  //     userUnLikeACat(cat.id);
-  //   }
-  // };
-
-  // {
-  //   /* Calculate the post time */
-  // }
-  // const getDateFormatted = (timestamp) => {
-  //   var date = new Date(timestamp);
-  //   var dateFormat = date.toDateString();
-  //   return dateFormat;
-  // };
+  const [data, setData] = useState([]);
 
   const showCatCardHandler = () => {
     setShowCatCard(!showCatCard);
@@ -173,21 +67,23 @@ export default function MapPage({ route, navigation }) {
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker
-          coordinate={{ latitude: 37.3387, longitude: -121.8853 }}
+        {/* <Marker
+          coordinate={{ latitude: 37.3307, longitude: -121.8853 }}
           onPress={showCatCardHandler}
-        >
-          <View>
-            {/* <Pressable> */}
-            <Foundation name="marker" size={40} color={Colors.orangeText} />
-            {/* </Pressable> */}
-          </View>
-          {/* <Callout>
+        > */}
+        {/* <View> */}
+        {/* <Pressable> */}
+        {/* <Foundation name="marker" size={40} color={Colors.orangeText} /> */}
+        {/* </Pressable> */}
+        {/* </View> */}
+        {/* <Callout>
             <View style={{ width: 100, height: 100, backgroundColor: "white" }}>
               <Text>Hi Hi hi</Text>
             </View>
           </Callout> */}
-        </Marker>
+        {/* </Marker> */}
+
+        <CatteryMarker />
       </MapView>
 
       {/* Header */}
@@ -202,10 +98,7 @@ export default function MapPage({ route, navigation }) {
         }}
       >
         <View style={{ top: 40, position: "absolute" }}>
-          <Pressable
-            // onPress={navigation.goBack}
-            style={{ marginLeft: 15 }}
-          >
+          <Pressable onPress={navigation.goBack} style={{ marginLeft: 15 }}>
             <Ionicons
               name="chevron-back"
               size={24}
@@ -248,26 +141,24 @@ export default function MapPage({ route, navigation }) {
       {/* <CatCard_map></CatCard_map>
       </ScrollView> */}
 
-      {showCatCard == true ? (
-        <View
-          style={{
-            height: 80,
-            backgroundColor: "transparent",
-            position: "absolute",
-            top: height - 170,
-          }}
-        >
-          {/* <View style={{ width: 200 }}></View> */}
-          <FlatList
-            // data={data}
-            renderItem={({ item }) => <CatCard_map cat={item} />}
-            horizontal
-          />
-          <CatCard_map></CatCard_map>
-        </View>
-      ) : (
-        <View />
-      )}
+      {/* Flatlist card */}
+      <View
+        style={{
+          height: 80,
+          backgroundColor: "transparent",
+          position: "absolute",
+          top: height - 170,
+          left: 30,
+        }}
+      >
+        {/* <View style={{ width: 200 }}></View> */}
+        <FlatList
+          // data={data}
+          renderItem={({ item }) => <CatCard_map cat={item} />}
+          horizontal
+        />
+        <CatCard_map></CatCard_map>
+      </View>
     </View>
   );
 }
@@ -278,7 +169,6 @@ const styles = StyleSheet.create({
     // flex: 1,
     justifyContent: "flex-end",
     alignItems: "flex-end",
-    // top: 130,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
