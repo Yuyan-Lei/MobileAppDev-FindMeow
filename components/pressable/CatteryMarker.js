9d905332269439b5e91bb1ee23f3e68c46614633
Yuyan-Lei <lei.yuy@northeastern.edu>
2022-12-06 00:02:23 -0800

    fix: rendering error in `CatteryMarker`

--- components/pressable/CatteryMarker.js
+++ components/pressable/CatteryMarker.js
@@ -56,6 +56,7 @@ export function CatteryMarker({
       {catsData.map((cat, index) => {
         return (
           <Marker
+            key={{index}}
             coordinate={{
               latitude: cat.geoLocation.lat,
               longitude: cat.geoLocation.lng,
