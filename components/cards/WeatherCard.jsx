8f1f125b8b1b6667f73bb51b60474d5b56905019
Yuyan-Lei <lei.yuy@northeastern.edu>
2022-12-09 18:12:00 -0800

    style: clean more codes

--- components/cards/WeatherCard.jsx
+++ components/cards/WeatherCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Colors } from "../styles/Colors";
 import { WEATHERS } from "../listContents/weathers";
